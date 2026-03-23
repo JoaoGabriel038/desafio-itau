09a74ef66d89b418c35ab73ac116dd0042a9e5a3
JoaoGabriel038 <joaogabriel038@gmail.com>
2026-03-23 13:19:29 -0300

    estatistica

---
+++ src/main/java/com/joaogabrieldev/transacao_api/controller/EstatiscasController.java
@@ -0,0 +1,24 @@
+package com.joaogabrieldev.transacao_api.controller;
+
+import com.joaogabrieldev.transacao_api.business.services.EstatisticasService;
+import com.joaogabrieldev.transacao_api.business.services.TransacaoService;
+import com.joaogabrieldev.transacao_api.controller.dtos.EstatisticasResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("estatistica")
+@RequiredArgsConstructor
+
+public class EstatiscasController {
+
+    private final EstatisticasService estatisticasService;
+
+    public ResponseEntity<EstatisticasResponseDTO> buscarEstatisticas(
+            @RequestParam(value = "IntervaloBusca", required = false, defaultValue = "60") Integer intervaloBusca ){
+        return ResponseEntity.ok(estatisticasService.calcularEstatisticasTransacoes(intervaloBusca));
+    }
+}
