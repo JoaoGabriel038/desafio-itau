04b722a25157690331a01abce5eb7959e52914aa
JoaoGabriel038 <joaogabriel038@gmail.com>
2026-03-23 13:11:53 -0300

    transacao

---
+++ src/main/java/com/joaogabrieldev/transacao_api/controller/TransacaoController.java
@@ -0,0 +1,30 @@
+package com.joaogabrieldev.transacao_api.controller;
+
+import com.joaogabrieldev.transacao_api.business.services.TransacaoService;
+import com.joaogabrieldev.transacao_api.controller.dtos.TransacaoRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/transacao")
+
+public class TransacaoController {
+
+    private final TransacaoService transacaoService;
+
+    @PostMapping
+    public ResponseEntity<Void> adicionarTransacao(@RequestBody TransacaoRequestDTO dto){
+        transacaoService.adicionarTransacao(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarTransacoes(){
+        transacaoService.limparTransacoes();
+        return ResponseEntity.ok().build();
+    }
+
+}
